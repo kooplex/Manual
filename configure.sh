a223e92d44da30aa4765fdeb57414c535345d1db
Visontai David <david.visontai@complex.elte.hu>
2021-02-03 15:12:19 +0000

    kubermetes version ready

--- configure.sh
+++ configure.sh
@@ -11,62 +11,57 @@ DOCKER_COMPOSE_FILE=$RF/docker-compose.yml
 case $VERB in
 
   "build")
-    echo "1. Configuring ${PREFIX}-manual..."
+    echo "1. Configuring ${PREFIX}-${MODULE_NAME}..."
+      mkdir_svclog
+      mkdir_svcdata
+
+      mkdir_svcdata _code_
+      HELPER_CODE_DIR=/data/manual/
+      #git clone https://github.com/kooplex/Manual.git /tmp/manual
+      kubectl cp /tmp/manual/docs helper:$HELPER_CODE_DIR
+
+      cp build/Dockerfile  $BUILDMOD_DIR/Dockerfile
+
+      docker $DOCKERARGS build -t ${PREFIX}-${MODULE_NAME} -f $BUILDMOD_DIR/Dockerfile $BUILDMOD_DIR
+      docker $DOCKERARGS tag ${PREFIX}-${MODULE_NAME} ${MY_REGISTRY}/${PREFIX}-${MODULE_NAME}
+      docker $DOCKERARGS push ${MY_REGISTRY}/${PREFIX}-${MODULE_NAME}
       
-      mkdir -p $SRV/_manual
-      docker $DOCKERARGS volume create -o type=none -o device=$SRV/_manual -o o=bind ${PREFIX}-manual
+      sed -e s,##PREFIX##,$PREFIX, \
+          -e s,##MODULE_NAME##,$MODULE_NAME, \
+	  build/${MODULE_NAME}-svcs.yaml-template > $BUILDMOD_DIR/${MODULE_NAME}-svcs.yaml
 
+      sed -e s,##PREFIX##,$PREFIX, \
+          -e s,##MODULE_NAME##,$MODULE_NAME, \
+          -e s,##SERVICENODE##,${SERVICE_NODE}, \
+          -e s,##MY_REGISTRY##,$MY_REGISTRY, \
+	  build/${MODULE_NAME}-pods.yaml-template > $BUILDMOD_DIR/${MODULE_NAME}-pods.yaml
 
-      cp Dockerfile  $RF
-      
-      sed -e "s/##PREFIX##/$PREFIX/" \
-          -e "s/##EXTRACONFIG##/$EXTRACONFIG/" docker-compose.yml-template > $DOCKER_COMPOSE_FILE
-
-      echo "2. Building ${PREFIX}-manual..."
-      docker-compose $DOCKER_HOST -f $DOCKER_COMPOSE_FILE build 
   ;;
   "install")
-      git clone https://github.com/kooplex/Manual.git $SRV/_manual
-  ;;
-  "install-hydra")
-  #  register_hydra $MODULE_NAME
-  ;;
-  "uninstall-hydra")
-   # unregister_hydra $MODULE_NAME
-  ;;
-  "install-nginx")
-    register_nginx $MODULE_NAME
-  ;;
-  "uninstall-nginx")
-    unregister_nginx $MODULE_NAME
+      #git clone https://github.com/kooplex/Manual.git $SRV/_manual
+      kubectl apply -f $BUILDMOD_DIR/${MODULE_NAME}-svcs.yaml
+      register_module_in_nginx
   ;;
 
   "start")
-    echo "Starting manual ${PREFIX}-manual"
-    docker-compose $DOCKERARGS -f $DOCKER_COMPOSE_FILE up -d
-  ;;
-
-  "restart")
-    echo "Restarting manual ${PREFIX}-manual"
-    docker $DOCKERARGS restart $PREFIX-manual
-  ;;
-
-  "init")
+    echo "Starting manual ${PREFIX}-${MODULE_NAME}"
+      kubectl apply -f $BUILDMOD_DIR/${MODULE_NAME}-pods.yaml
   ;;
     
   "stop")
-    echo "Stopping manual ${PREFIX}-manual"
-    docker-compose $DOCKERARGS -f $DOCKER_COMPOSE_FILE down
+    echo "Stopping manual ${PREFIX}-${MODULE_NAME}"
+      kubectl delete -f $BUILDMOD_DIR/${MODULE_NAME}-pods.yaml
   ;;
     
   "remove")
-    echo "Removing manual ${PREFIX}-net"
-    docker-compose $DOCKERARGS -f $DOCKER_COMPOSE_FILE kill
-    docker-compose $DOCKERARGS -f $DOCKER_COMPOSE_FILE rm
+    echo "Removing manual ${PREFIX}-${MODULE_NAME}"
+      deregister_module_in_nginx
+      echo "Deleting services of ${PREFIX}-${MODULE_NAME}" >&2
+      kubectl delete -f $BUILDMOD_DIR/${MODULE_NAME}-svcs.yaml || true
   ;;
     
   "purge")
-    echo "Purging manual ${PREFIX}-manual"
+    echo "Purging manual ${PREFIX}-${MODULE_NAME}"
     rm -R $RF
 
     docker $DOCKERARGS volume rm ${PREFIX}-manual
